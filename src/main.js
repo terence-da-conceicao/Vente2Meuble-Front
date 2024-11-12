735b8cb14e39a19d4ce98839ee532851773747d1
Paul Vaucher <vaucher.paul@gmx.com>
2024-11-12 16:19:29 +0100

    ajout du fetch dans main.js après modif back

--- src/main.js
+++ src/main.js
@@ -9,3 +9,15 @@ const app = createApp(App)
 app.use(router)
 app.mount('#app')
 
+let meubles_back = fetch('http://localhost:8000/meubles')
+    .then(response => response.json())
+    .then(data => {
+        console.log('Meubles récupérés:', data);
+        return data
+      })
+      .catch(error => {
+        console.error('Erreur de récupération:', error);
+        return meubles_back;
+      })
+      
+console.log (meubles_back.meubles.id)
